9c0da5fed65d615531c96c494db9fc514d64492b
John Galvin <jgalvin@backupearth.com>
2024-11-10 14:18:57 +0000

    Added Tiled Map Projects, before creating new header file to handle all of tiledmap projects

---
+++ JohnnyGotTheRuns/assets/tiledprojects/Industrial35x35.tsx
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Industrial35x35" tilewidth="35" tileheight="35" tilecount="448" columns="28">
+ <image source="../images/levelSpriteSheet.png" width="980" height="560"/>
+ <tile id="236" type="WizRect">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="35" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="238">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Rect" x="0.666667" y="0" width="33.3333" height="20"/>
+  </objectgroup>
+ </tile>
+ <tile id="242">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Point" x="16" y="22">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="244">
+  <objectgroup draworder="index" id="3">
+   <object id="4" type="Elipese" x="0.666667" y="4.66667" width="33.3333" height="26.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="298">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Triangle" x="0.666667" y="0.666667">
+    <polygon points="0,0 32.6667,33.3333 -0.666667,32.6667"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="345">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Polyon" x="-1.33333" y="20">
+    <polygon points="0,0 26.6667,0.666667 36.6667,13.3333 2,14.6667"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
